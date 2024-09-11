678e25ffa79ef5fed588091394d1e65f445c7dad
Ondra Hlaváč <ondra@hlavac.cz>
2024-09-11 20:09:11 +0200

    feat: allow user to select theme mode in the About section of the app.

--- lib/screens/about_page.dart
+++ lib/screens/about_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:learning_flutter_riverpod/providers/theme_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class AboutPage extends StatelessWidget {
@@ -28,9 +30,41 @@ class AboutPage extends StatelessWidget {
                   'https://github.com/ondrahlavac/learning-flutter-riverpod'),
               child: const Text('View on GitHub'),
             ),
+            const SizedBox(height: 20),
+            Text(
+              'Customize your experience',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            const SizedBox(height: 20),
+            const ThemeToggleButtons(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class ThemeToggleButtons extends ConsumerWidget {
+  const ThemeToggleButtons({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeProvider).themeMode;
+
+    return ToggleButtons(
+      isSelected: [
+        themeMode == ThemeMode.system,
+        themeMode == ThemeMode.light,
+        themeMode == ThemeMode.dark,
+      ],
+      onPressed: (int index) {
+        ref.read(themeProvider.notifier).setThemeMode(ThemeMode.values[index]);
+      },
+      children: const [
+        Icon(Icons.brightness_auto),
+        Icon(Icons.light_mode),
+        Icon(Icons.dark_mode),
+      ],
     );
   }
 }
