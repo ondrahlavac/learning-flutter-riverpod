cc2212adbd862787da36e73c29c97e28d3d825b9
Ondra Hlaváč <ondra@hlavac.cz>
2024-09-11 20:09:11 +0200

    feat: allow user to select theme mode in the About section of the app
    Also make the whole about section a little bit nicer and scrollable.

--- lib/screens/about_page.dart
+++ lib/screens/about_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:learning_flutter_riverpod/providers/theme_provider.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class AboutPage extends StatelessWidget {
@@ -15,22 +17,101 @@ class AboutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'This is a simple app for learning Flutter Riverpod.',
-              textAlign: TextAlign.center,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Learning Fluttter Riverpod',
+                  style: Theme.of(context).textTheme.displayLarge,
+                  textAlign: TextAlign.left,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'This is a simple app for learning Flutter Riverpod.',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                  textAlign: TextAlign.left,
+                ),
+                const SizedBox(height: 20),
+                TextButton(
+                  onPressed: () => _launchUrl(
+                      'https://github.com/ondrahlavac/learning-flutter-riverpod'),
+                  child: const Text(
+                    'View on GitHub',
+                    textAlign: TextAlign.left,
+                  ),
+                ),
+                const SizedBox(height: 60),
+                Text(
+                  'Customize your experience',
+                  style: Theme.of(context).textTheme.titleLarge,
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Select the application theme',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+                const SizedBox(height: 10),
+                const ThemeToggleButtons(),
+                const SizedBox(height: 20),
+              ],
             ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: () => _launchUrl(
-                  'https://github.com/ondrahlavac/learning-flutter-riverpod'),
-              child: const Text('View on GitHub'),
-            ),
-          ],
+          ),
         ),
       ),
+    );
+  }
+}
+
+class ThemeToggleButtons extends ConsumerWidget {
+  const ThemeToggleButtons({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(themeProvider).themeMode;
+
+    return ToggleButtons(
+      isSelected: [
+        themeMode == ThemeMode.system,
+        themeMode == ThemeMode.light,
+        themeMode == ThemeMode.dark,
+      ],
+      onPressed: (int index) {
+        ref.read(themeProvider.notifier).setThemeMode(ThemeMode.values[index]);
+      },
+      borderRadius: BorderRadius.circular(24.0),
+      children: const <Widget>[
+        Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              Icon(Icons.brightness_auto),
+              Text('System'),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              Icon(Icons.light_mode),
+              Text('Light'),
+            ],
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              Icon(Icons.dark_mode),
+              Text('Dark'),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
